976e036d57cef932bd5706553593846c0a6fddf4
thezhi <2263796656@qq.com>
2025-03-14 01:13:56 +0800

    指令寄存器修正

--- Module_CPU/InstructionMemory.v
+++ Module_CPU/InstructionMemory.v
@@ -9,9 +9,9 @@ module InstructionMemory(
     output reg[3:0] rs,    //源操作数1
     output reg[3:0] rt,    //源操作数2
     output reg[3:0] rd,    //目的操作数
-    output reg[3:0] imm    //立即数
+    output reg[15:0] instruction //传递给立即数扩展模块的指令
 );
-    reg[7:0] mem[0:63]; //mem 用于存储指令数据，总共有 64 个 8 位的元素。
+    reg[15:0] mem[0:63]; //mem 用于存储指令数据，总共有 64 个 16 位的元素。
 
     initial 
      begin
@@ -20,97 +20,11 @@ module InstructionMemory(
 
 
     always @(*)begin
-        // 从地址取值然后输出
-        op = mem[IAddress + 1][3:0];
-        case (op)
-            4'b0000:begin   //jal
-                rd = mem[IAddress + 1][7:4];
-                imm = mem[IAddress][3:0];
-// 0000 0010 0001 0000
-            end
-            4'b0001:begin   //jalr
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4];
-            end
-            4'b0010:begin   //beq
-                imm = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                rt = mem[IAddress][7:4];
-            end
-            4'b0011:begin   //ble
-                imm = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                rt = mem[IAddress][7:4];
-            end
-            4'b0100:begin   //lb
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4];
-            end
-            4'b0101:begin   //lw
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4];    
-            end
-            4'b0110:begin   //sb
-                 rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4];                    
-            end
-            4'b0111:begin   //sw
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4]; 
-            end
-            4'b1000:begin   //add
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                rt = mem[IAddress][7:4]; 
-            end
-            4'b1001:begin   //sub
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                rt = mem[IAddress][7:4]; 
-
-            end
-            4'b1010:begin   //and
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                rt = mem[IAddress][7:4]; 
-                
-            end
-            4'b1011:begin   //or
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                rt = mem[IAddress][7:4]; 
-                
-            end
-            4'b1100:begin   //addi
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4]; 
-                
-            end
-            4'b1101:begin   //subi
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4];    
-                
-            end
-            4'b1110:begin   //andi
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4]; 
-            end
-            4'b1111:begin   //ori
-                rd = mem[IAddress + 1][7:4];
-                rs = mem[IAddress][3:0];
-                imm = mem[IAddress][7:4]; 
-                
-            end
-
-        endcase
+        op = mem[IAddress][3:0];
+        rd = mem[IAddress][7:4];
+        rs = mem[IAddress][11:8];
+        rt = mem[IAddress][15:12];
+        instruction = mem[IAddress];
     end
 
 
