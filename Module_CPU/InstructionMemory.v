3ab69fc653989e3873b074e10515eab4f6d503b3
thezhi <2263796656@qq.com>
2025-03-27 20:03:40 +0800

    指令寄存器修改

--- Module_CPU/InstructionMemory.v
+++ Module_CPU/InstructionMemory.v
@@ -1,7 +1,9 @@
 
 // InstructionMemory：储存指令，分割指令
+// 指令地址是以字节为单位的，而内存地址是以字为单位的。
+// 所以，指令地址需要右移一位才能得到对应的内存地址。
 module InstructionMemory(
-    input InsMemRW,     //读写信号，1为写，0为读
+    // input InsMemRW,     //读写信号，1为写，0为读
     input [15:0] IAddress,   //指令地址输入入口
     // input [15:0] IData,   //指令数据
 
@@ -14,17 +16,17 @@ module InstructionMemory(
     reg[15:0] mem[0:63]; //mem 用于存储指令数据，总共有 64 个 16 位的元素。
 
     initial 
-     begin
+    begin
         $readmemb("D:/learn/Git/testgit/test/test.txt", mem);  //读取测试文档中的指令
-     end
+    end
 
 
     always @(*)begin
-        op = mem[IAddress][3:0];
-        rd = mem[IAddress][7:4];
-        rs = mem[IAddress][11:8];
-        rt = mem[IAddress][15:12];
-        instruction = mem[IAddress];
+        op = mem[IAddress >> 1][3:0];
+        rd = mem[IAddress >> 1][7:4];
+        rs = mem[IAddress >> 1][11:8];
+        rt = mem[IAddress >> 1][15:12];
+        instruction = mem[IAddress >> 1];
     end
 
 
