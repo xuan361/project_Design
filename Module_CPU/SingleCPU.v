b41da0de5f8a6cd1435979c2026de08a7402e024
thezhi <2263796656@qq.com>
2025-03-27 20:41:33 +0800

    顶层模块，但愿成功

--- Module_CPU/SingleCPU.v
+++ Module_CPU/SingleCPU.v
@@ -38,17 +38,14 @@ module SingleCPU(
 
     // 各种临时变量
     wire [2:0] ALUOp; 
-    wire m2reg, wmem, alucsrc, wreg, jal;
+    wire m2reg, wmem, alucsrc, wreg, jal,memc;  // 控制信号 
     wire[1:0] PCsrc;
     wire [15:0] newAddress;
-    wire [15:0] currentAddress_2, currentAddress_imm;
-    wire InsMemRW;
+    wire [15:0] currentAddress_2, currentAddress_immediate;
     wire [15:0] B;
     wire [15:0] immExt; 
-    wire [3:0] WriteReg;  
-
-    wire memc;
     wire [15:0] instruction;
+    wire [15:0] back_regiser;
 
 
 // 各模块实例化
@@ -59,14 +56,13 @@ module SingleCPU(
     PC pc(CLK, RESET, newAddress, currentAddress);
 
     // InstructionMemory：储存指令，分割指令
-    InstructionMemory im(InsMemRW, currentAddress, op, rs, rt, rd, instruction);
+    InstructionMemory im(currentAddress, op, rs, rt, rd, instruction);
     
     // ImmExt: 用于immediate的扩展
     ImmExt ImmE(instruction, immExt);
 
     //RegisterFile：储存寄存器组，并根据地址对寄存器组进行读写
-    //要改！！！
-    RegisterFile rf(CLK, wreg, rs, rt, WriteReg, WriteData, ReadData1, ReadData2);
+    RegisterFile rf(CLK, wreg, rs, rt, rd, WriteData, ReadData1, ReadData2);
 
     //ALU（算术逻辑单元）：用于逻辑指令计算和跳转指令比较
     ALU alu(ALUOp, ReadData1, B,  result, zero);
@@ -74,7 +70,7 @@ module SingleCPU(
 
 
     // DataMemory：用于内存存储，内存读写
-    DataMemory DM(wmem, result, ReadData2, memc, DataOut);
+    DataMemory DM(clk, wmem, result, ReadData2, memc, DataOut);
 
     assign currentAddress_2 = currentAddress + 2;
     assign currentAddress_immediate = currentAddress + immExt;
@@ -83,16 +79,14 @@ module SingleCPU(
     Multiplexer21 m21_0(alucsrc, ReadData2, ImmE, B);
     
     // 选择写回寄存器组的数据来源，ALU的结果或存储器的数据
-    Multiplexer21 m21_1(m2reg, result, DataOut, WriteData);
+    Multiplexer21 m21_1(m2reg, result, DataOut, back_regiser);
 
-    // 为寄存器组选择数据来源，WriteData或rcurrentAddress_2
-    Multiplexer21 m21_2(jal, WriteData, rcurrentAddress_2, WriteReg);
+    // 为寄存器组选择数据来源，WriteData或currentAddress_2
+    Multiplexer21 m21_2(jal, back_regiser, currentAddress_2, WriteData);
 
     // 选择写回PC的数据来源，PC+2 或 PC+immExt 或 result
-    Multiplexer31 m31(PCsrc, currentAddress, immExt, result, newAddress);
+    Multiplexer31 m31(PCsrc, currentAddress, currentAddress_immediate, result, newAddress);
 
-
-// 下面才是要进行操作的代码
 
 
 
