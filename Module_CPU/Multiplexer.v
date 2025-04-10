93262314b50034a201684171c1b95c9cc34d66e7
thezhi <2263796656@qq.com>
2025-04-10 20:11:01 +0800

    多路选择器：删去原来写进寄存器的二路选择器，新增4路选择器作为写回寄存器来源

--- Module_CPU/Multiplexer.v
+++ Module_CPU/Multiplexer.v
@@ -1,8 +1,10 @@
 
-// Multiplexer2：二路选择器
-// Multiplexer3：三路选择器
+// Multiplexer21：二路选择器
+// Multiplexer31：三路选择器
+// Multiplexer41：四路选择器
 
 
+// 选择立即数，还是B_data写入到ALU
 module Multiplexer21 (
     input control,
     input [15:0] in0,
@@ -10,7 +12,6 @@ module Multiplexer21 (
     output [15:0] out
 
 );
-
     assign out = control ? in1 : in0;
 endmodule
 
@@ -32,5 +33,30 @@ module Multiplexer31 (
 
     end
 
+
+endmodule
+
+// 选择写回寄存器的数据
+// 0：把ALU的运算结果传回
+// 1：把数据存储器的数据传回 
+// 2:把currentAddress_2传回 
+// 3：把立即数左移八位后的数据传回
+module Multiplexer41 (
+    input [1:0] control,
+    input [15:0] in0,
+    input [15:0] in1,
+    input [15:0] in2,
+    input [15:0] in3,
+
+    output reg[15:0] out
+);
+    always @(*)begin
+        case(control)
+            0:  out = in0;
+            1:  out = in1;
+            2:  out = in2;
+            3:  out = in3;
+        endcase
+    end
 
 endmodule
