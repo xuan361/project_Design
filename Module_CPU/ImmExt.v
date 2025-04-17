6c92868936b3adb42aeda3c3223d18c63bd8b6fc
thezhi <2263796656@qq.com>
2025-04-17 23:31:47 +0800

    修改涉及跳转的立即数逻辑

--- Module_CPU/ImmExt.v
+++ Module_CPU/ImmExt.v
@@ -14,19 +14,22 @@ always @(*)begin
     op = instruction[3:0];
     case(op)    
         4'b0000:begin   //jal
-            imm_8 = instruction[15:8];
-            immExt[15:8] = imm_8[7] ? 8'b1111_1111 : 0;
-            immExt[7:0] = imm_8;
+            imm_8 = (instruction[15:8]);
+            // immExt[15:8] = imm_8[7] ? 8'b1111_1111 : 0;
+            // immExt[7:0] = imm_8;
+            immExt = {{7{imm_8[7]}}, imm_8, 1'b0};
         end
         4'b0001:begin   //jalr
             imm_4 = instruction[15:12];
-            immExt[15:4] = imm_4[3] ? 12'b1111_1111_1111 : 0;
-            immExt[3:0] = imm_4;            
+            // immExt[15:4] = imm_4[3] ? 12'b1111_1111_1111 : 0;
+            // immExt[3:0] = imm_4;
+            immExt = {{11{imm_4[3]}}, imm_4, 1'b0};                        
         end
         4'b0010, 4'b0011:begin  //beq, ble
             imm_4 = instruction[7:4];
-            immExt[15:4] = imm_4[3] ? 12'b1111_1111_1111 : 0;
-            immExt[3:0] = imm_4; 
+            // immExt[15:4] = imm_4[3] ? 12'b1111_1111_1111 : 0;
+            // immExt[3:0] = imm_4; 
+            immExt = {{11{imm_4[3]}}, imm_4, 1'b0};   
         end
 
         4'b0100, 4'b0101:begin  //lb, lw
